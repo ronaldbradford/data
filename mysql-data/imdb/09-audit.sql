c851047548b9e8482a607cf5badd495d3d335712
Ronald Bradford <ronald.bradford@gmail.com>
2025-02-28 21:08:55 -0500

    Removed crew as included in principal

--- mysql-data/imdb/09-audit.sql
+++ mysql-data/imdb/09-audit.sql
@@ -5,10 +5,3 @@ SELECT CURDATE(),
        LENGTH(known_for) - LENGTH(REPLACE(known_for, ',', ''))+1 AS known_for_count,
        MAX(LENGTH(known_for)) AS known_for_length
 FROM   name;
-
-SELECT MAX(LENGTH(directors)),
-       MAX(LENGTH(writers)) 
-FROM title_crew;
-
-SELECT MAX(LENGTH(directors) - LENGTH(REPLACE(directors,',','')))+1 AS director_count FROM title_crew;
-SELECT MAX(LENGTH(writers) - LENGTH(REPLACE(writers,',','')))+1 AS writer_count FROM title_crew;
